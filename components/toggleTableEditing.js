7479be593d129509dfff46450b3f6659cf3d3231
pin-cup <user@mailfence.com>
2021-09-04 17:52:47 +0300

    Remove useless

--- components/toggleTableEditing.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-function toggleTableEditing({ bool = false, Container = document }) {
-	const inps = Container.querySelectorAll("input");
-
-	bool && inps.forEach((i) => i.removeAttribute("readOnly"));
-	bool || inps.forEach((i) => (i.readOnly = true));
-}

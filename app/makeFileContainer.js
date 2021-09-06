047a31d2299858ee555585032c10f47f95294c69
pin-cup <user@mailfence.com>
2021-09-07 00:14:14 +0300

    Move in new dir

---
+++ app/makeFileContainer.js
@@ -0,0 +1,21 @@
+"use strict";
+
+function makeFileContainer({ name, size, link }) {
+	const //
+		href = link ? `href="${link}"` : "",
+		download = link ? `download="${name}"` : "",
+		NclassName = 'class="file_name"',
+		SclassName = 'class="file_size"',
+		namevalue = name || "Empty",
+		sizevalue = size || "...",
+		namehtml = `<a ${href} ${download} ${NclassName}">${namevalue}</a>`,
+		sizehtml = `<i ${SclassName}> ${sizevalue}KB</i>`,
+		html = namehtml + sizehtml,
+		File = document.createElement("div");
+
+	File.classList = "file-container-element";
+	File.insertAdjacentHTML("beforeend", html);
+	File.addEventListener("click", handler_onFileButton, false);
+
+	return File;
+}

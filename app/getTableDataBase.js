a00290d625c28e4f5194a0bd63dae67cf34ba70c
pin-cup <user@mailfence.com>
2021-09-11 18:22:56 +0300

    Add comments

--- app/getTableDataBase.js
+++ app/getTableDataBase.js
@@ -1,32 +1,26 @@
 "use strict";
 
-/**
- * @returns {Array} Array [{},{},{}]
- */
- function getTableDataBase() {
+const getTableDataBase = ({ place }) => {
 	const //
-		database = [],
-		g = (str, obj = document) => obj.querySelector(str),
-		gl = (str, obj) => obj.querySelectorAll(str),
-		Container = g(".table-container"),
-		Rows = gl(".row", Container);
+		Container = place,
+		Rows = Container.querySelectorAll(".row"),
+		new_db = [];
 
 	Rows.forEach((Row) => {
-		const //
-			array = [],
-			Cells = gl(".cell", Row);
-
+		const Cells = Row.querySelectorAll(".cell");
+		const new_object = {};
 		Cells.forEach((Cell) => {
 			const //
-				_ = (str) => g(str, Cell).value,
-				inp_name = _(".cell__name"),
-				inp_value = _(".cell__value");
-
-			array.push([inp_name, inp_value]);
+				valOf = (str) => Cell.querySelector(str).value,
+				// Get name & value
+				inp_name = valOf(".cell__name"),
+				inp_value = valOf(".cell__value");
+			// Add make object
+			new_object[inp_name] = inp_value;
 		});
-
-		database.push(Object.fromEntries(array));
+		// Add new object in array
+		new_db.push(new_object);
 	});
 
-	return database;
-}
+	return new_db;
+};
